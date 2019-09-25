fd3f9adcf443a83ee1157513bde09090c697bc21
Everton Xavier <everton19.xavier@gmail.com>
2019-09-25 13:41:30 -0400

    Descomentado lógica de QRcode e atualizado com upstream

--- src/Common/Tools.php
+++ src/Common/Tools.php
@@ -333,10 +333,10 @@ class Tools
         if ($modelo == 67) {
             $method = 'cteOS';
         }
-        /* $isInfCTeSupl = !empty($dom->getElementsByTagName('infCTeSupl')->item(0));
+        $isInfCTeSupl = !empty($dom->getElementsByTagName('infCTeSupl')->item(0));
         if (!$isInfCTeSupl) {
             $signed = $this->addQRCode($dom);
-        } */
+        }
         $this->isValid($this->versao, $signed, $method);
         $modal = (int) $dom->getElementsByTagName('modal')->item(0)->nodeValue;
         if ($modelo != 67) {
